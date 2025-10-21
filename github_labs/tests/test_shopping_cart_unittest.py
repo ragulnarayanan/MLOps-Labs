65b7e2b2ef9563c51c4329dd1177f0067078fd5a
Ragul Narayanan <ragulnarayananm@gmail.com>
2025-10-20 22:11:50 -0400

    Update unittest file

--- github_labs/tests/test_shopping_cart_unittest.py
+++ github_labs/tests/test_shopping_cart_unittest.py
@@ -9,23 +9,23 @@ class TestShoppingCart(unittest.TestCase):
 
     def test_add_item(self):
         self.cart.add_item("Apple", 1.0, 2)
-        self.assertEqual(len(self.cart.items), 1)
-        self.assertEqual(self.cart.items[0]["item_name"], "Apple")
+        self.assertAlmostEqual(len(self.cart.items), 1)
+        self.assertAlmostEqual(self.cart.items[0]["item_name"], "Apple")
 
     def test_remove_item(self):
         self.cart.add_item("Banana", 0.5, 3)
         self.cart.remove_item("Banana")
-        self.assertEqual(len(self.cart.items), 0)
+        self.assertAlmostEqual(len(self.cart.items), 0)
 
     def test_calculate_total(self):
         self.cart.add_item("Apple", 1.0, 2)
         self.cart.add_item("Banana", 0.5, 3)
-        self.assertEqual(self.cart.calculate_total(), 3.5)
+        self.assertAlmostEqual(self.cart.calculate_total(), 3.5)
 
     def test_apply_discount(self):
         self.cart.add_item("Apple", 1.0, 2)
         self.cart.add_item("Banana", 0.5, 3)
-        self.assertEqual(self.cart.apply_discount(20), 2.8)
+        self.assertAlmostEqual(self.cart.apply_discount(20), 2.8)
 
     def test_invalid_price_or_quantity(self):
         with self.assertRaises(ValueError):
